5854fa5061bfc9560d9f2df73fd15a70a9b59f37
Nicholas <nickcfeldman@gmail.com>
2016-05-02 10:55:50 -0400

    Log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root             'static_pages#home'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 

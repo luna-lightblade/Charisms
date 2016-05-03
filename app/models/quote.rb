74fc291155c65b080b0fbbb72c06775db0f59980
Nicholas <nickcfeldman@gmail.com>
2016-05-02 23:37:44 -0400

    Small fixes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :quote, presence: true, length: { maximum: 140 }
+  validates :quote, presence: true, length: { maximum: 140 }, uniqueness: true
   validates :context, length: { maximum: 250 }
   validates :sayer, presence: true, length: { maximum: 50 }
 end

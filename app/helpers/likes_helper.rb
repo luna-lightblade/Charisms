208696f1bd077f3aefeacb0e18de9d4fb0bbc520
Nicholas <nickcfeldman@gmail.com>
2016-05-04 19:50:50 -0400

    Fix Like/Likes based on amount

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -3,4 +3,8 @@ module LikesHelper
   def has_liked?(quote)
     Like.where(:user_id => current_user.id, :quote_id => quote.id).count > 0
   end
+
+  def like_count(quote)
+    Like.where(:quote_id => quote.id).count
+  end
 end
